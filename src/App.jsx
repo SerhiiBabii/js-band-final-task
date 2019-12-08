7d3254eb2560128ff3e896871c806bfceab14a4c
Serhii Babii <babiysergey1988@gmail.com>
2019-12-09 00:18:09 +0200

    FT-6: + BookList

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Cart from './components/Cart/Cart'
 import Book from './components/Book/Book'
 import SignIn from './components/SignIn/SignIn'
 import NotFound from './components/NotFound/NotFound'
+import BookList from './components/BookList/BookList'
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
       <Book />
       <SignIn />
       <NotFound />
+      <BookList />
     </div>
   );
 }
