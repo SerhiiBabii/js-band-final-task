82b666c83d87554d2c9ec77b33b7badc88a4f6d6
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-08 10:03:55 +0000

    Deploy SerhiiBabii/js-band-final-task to github.com/SerhiiBabii/js-band-final-task.git:gh-pages

---
+++ precache-manifest.6bfba39c825dca5b9d9ab1dc28e6797a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a91a7a042a422703a6a5d1ab7724b92d",
+    "url": "/js-band-final-task/index.html"
+  },
+  {
+    "revision": "66da9119261213e76bad",
+    "url": "/js-band-final-task/static/js/2.32205984.chunk.js"
+  },
+  {
+    "revision": "6d255cf5a8f3d7ba642b",
+    "url": "/js-band-final-task/static/js/main.c594178f.chunk.js"
+  },
+  {
+    "revision": "49af006cc0cf04963d6b",
+    "url": "/js-band-final-task/static/js/runtime-main.3be8268d.js"
+  }
+]);
