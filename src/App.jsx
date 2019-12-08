d44c02d43a47e39ab3bc0e8ba21b5102db718e6a
Serhii Babii <babiysergey1988@gmail.com>
2019-12-08 18:25:44 +0200

    FT-3: + Book

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from "./components/Header/Header";
 import Cart from './components/Cart/Cart'
+import Book from './components/Book/Book'
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Header />
       <hr />
       <Cart />
+      <Book />
     </div>
   );
 }
