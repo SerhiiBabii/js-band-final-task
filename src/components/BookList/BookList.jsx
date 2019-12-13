c6a8fc2c34c1917f0a8613ebad4c2b7561cc90d6
Serhii Babii <babiysergey1988@gmail.com>
2019-12-13 13:22:46 +0200

    FT-13: * test list, PropTypes

--- src/components/BookList/BookList.jsx
+++ src/components/BookList/BookList.jsx
@@ -1,42 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import BookItem from './BookItem';
 import Filters from '../Filters/Filters'
 
-const BookList = () => {
-  // array below for testing book list
-  const cart = [
-    {
-      id: 1,
-      cover: '',
-      title: 'Title 1',
-      author: 'Author 1',
-      price: 32,
-    },
-    {
-      id: 2,
-      cover: '',
-      title: 'Title 2',
-      author: 'Author 2',
-      price: 18,
-    },
-    {
-      id: 3,
-      cover: '',
-      title: 'Title 3',
-      author: 'Author 3',
-      price: 12,
-    },
-  ]
+const BookList = ({books}) => {
+
   return (
     <div className="row p-3">
       <div className="col-10">
         <Filters />
       </div>
       <div className="row">
-        {cart.map((item)=> <BookItem key={item.id} book={item} />)}
+        {books.map((item)=> <BookItem key={item.id} book={item} />)}
       </div>
     </div>
   )
+}
+
+BookList.propTypes = {
+  books: PropTypes.instanceOf(Array).isRequired,
 }
 
 export default BookList;
