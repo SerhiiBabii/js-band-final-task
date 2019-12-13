0b86bbb093c7a95896eca3356097b2a39756e235
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-13 13:50:53 +0000

    Deploy SerhiiBabii/js-band-final-task to github.com/SerhiiBabii/js-band-final-task.git:gh-pages

---
+++ precache-manifest.4f01b001af2ee1f37f0154433d6020d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea155a92ab286411d0e1856aeea5d38f",
+    "url": "/js-band-final-task/index.html"
+  },
+  {
+    "revision": "39a839abd4ce8b09dac4",
+    "url": "/js-band-final-task/static/css/main.7a1ef6fc.chunk.css"
+  },
+  {
+    "revision": "2f69df88eb3a7b82b667",
+    "url": "/js-band-final-task/static/js/2.9fab5e1e.chunk.js"
+  },
+  {
+    "revision": "39a839abd4ce8b09dac4",
+    "url": "/js-band-final-task/static/js/main.6e3e7296.chunk.js"
+  },
+  {
+    "revision": "49af006cc0cf04963d6b",
+    "url": "/js-band-final-task/static/js/runtime-main.3be8268d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/js-band-final-task/precache-manifest.fc0e19d7a779f62ea8ef93d59c86ba8e.js"
+  "/js-band-final-task/precache-manifest.4f01b001af2ee1f37f0154433d6020d9.js"
 );
 
 self.addEventListener('message', (event) => {
