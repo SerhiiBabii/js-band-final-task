532aeb1e62301e1b4fd91269eab12533f62242b2
Serhii Babii <babiysergey1988@gmail.com>
2019-12-13 09:24:35 +0200

    FT-12: + PropTypes, user state, signOut action

--- src/components/Header/HeaderUser.jsx
+++ src/components/Header/HeaderUser.jsx
@@ -1,17 +1,35 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
+import {signOut} from '../../actions/actions';
 
-const HeaderUser = () => {
+const HeaderUser = ({user, signOutUser}) => {
+  const {username, avatar} = user;
+
   return (
     <div>
       <Link to="/cart">
         <img height="40" src="./images/cart.svg" alt="cart" />
       </Link>
-      <button type="button" className="btn btn-primary m-2">Sing-Out</button>
-      <img height="40" className="m-2 rounded-circle" src="" alt="avatar" />
-      <span>Username</span>
+      <button onClick={signOutUser} type="button" className="btn btn-primary m-2">Sing-Out</button>
+      <img height="40" className="m-2 rounded-circle" src={avatar} alt="avatar" />
+      <span>{username}</span>
     </div>
   )
 }
 
-export default HeaderUser;
+HeaderUser.propTypes = {
+  user: PropTypes.instanceOf(Object).isRequired,
+  signOutUser: PropTypes.func.isRequired,
+}
+
+const mapStateToProps = (state) => ({
+  user: state.books.user,
+})
+
+const mapDispatchToProps = (dispatch) => ({
+  signOutUser: () => dispatch(signOut()),
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(HeaderUser);
