103237aadff0ed6d056866e83c3c584668e20d33
Serhii Babii <babiysergey1988@gmail.com>
2019-12-13 13:26:23 +0200

    FT-13: + PropTypes, state(book, token, loading), actions

--- src/components/BookList/BookList.jsx
+++ src/components/BookList/BookList.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
 import BookItem from './BookItem';
-import Filters from '../Filters/Filters'
+import Filters from '../Filters/Filters';
+import {booksRequest, booksSuccess, booksFailure} from '../../actions/actions';
 
-const BookList = ({books}) => {
+const BookList = ({books, token, loading, fetchBooksRequest, fetchBooksSuccess, fetchBooksFailure}) => {
 
   return (
     <div className="row p-3">
@@ -19,6 +21,24 @@ const BookList = ({books}) => {
 
 BookList.propTypes = {
   books: PropTypes.instanceOf(Array).isRequired,
+  token: PropTypes.string.isRequired,
+  loading: PropTypes.bool.isRequired,
+  fetchBooksRequest: PropTypes.func.isRequired,
+  fetchBooksSuccess: PropTypes.func.isRequired,
+  fetchBooksFailure: PropTypes.func.isRequired,
 }
 
-export default BookList;
+const mapStateToProps = (state) => ({
+  books: state.books.books,
+  token: state.books.user.token,
+  loading: state.books.loading,
+})
+
+const mapDispatchToProps = (dispatch) => ({
+  fetchBooksRequest: () => dispatch(booksRequest()),
+  fetchBooksSuccess: (books) => dispatch(booksSuccess(books)),
+  fetchBooksFailure: (error) => dispatch(booksFailure(error)),
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
+
