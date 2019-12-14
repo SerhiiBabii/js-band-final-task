1a23f118f7037903930157b986328e9517d922d6
Serhii Babii <babiysergey1988@gmail.com>
2019-12-14 17:15:16 +0200

    FT-14: * orderTotal

--- src/components/Cart/CartTotalPrice.jsx
+++ src/components/Cart/CartTotalPrice.jsx
@@ -1,30 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const CartTotalPrice = (props) => {
-  const {cart} = props;
-  let totalPrice = 0;
-  if(cart) {
-    cart.forEach((item) => {
-      totalPrice += +item.totalPriceBook
-    })
-  }
+const CartTotalPrice = ({orderTotal}) => {
   return (
     <div>
       <b>
         Total price, $
-        {totalPrice}
+        {orderTotal}
       </b>
     </div>
   )
 }
 
-CartTotalPrice.defaultProps = {
-  cart: undefined,
-}
-
 CartTotalPrice.propTypes = {
-  cart: PropTypes.instanceOf(Object),
+  orderTotal: PropTypes.string.isRequired,
 }
 
 export default CartTotalPrice;
