f66fb4920c8e45e541beefc13811e91121ee55d9
Serhii Babii <babiysergey1988@gmail.com>
2019-12-14 19:13:35 +0200

    FT-15: + showFilteredBooks, search value, price value

--- src/components/Filters/Filters.jsx
+++ src/components/Filters/Filters.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
+import {showFilteredBooks} from '../../actions/actions';
 
-const Filters = () => {
+const Filters = ({search, price, onChangeFilter}) => {
   return (
     <div className="form-group row">
-      <input className="form-control col-6" type="text" name="search" placeholder="Search by book name" />
-      <select className="form-control col-3" name="price">
+      <input
+        onChange={(e) => onChangeFilter(e.target.name, e.target.value)}
+        value={search}
+        className="form-control col-6"
+        type="text"
+        name="search"
+        placeholder="Search by book name"
+      />
+      <select onChange={(e) => onChangeFilter(e.target.name, e.target.value)} value={price} className="form-control col-3" name="price">
         <option value="all">all</option>
         <option value="from_0_to_15">{'0 < price > 15'}</option>
         <option value="from_15_to_30">{'15 < price > 30'}</option>
@@ -14,4 +24,19 @@ const Filters = () => {
   )
 }
 
-export default Filters;
+Filters.propTypes = {
+  search: PropTypes.string.isRequired,
+  price: PropTypes.string.isRequired,
+  onChangeFilter: PropTypes.func.isRequired,
+}
+
+const mapStateToProps = (state) => ({
+  search: state.books.filters.search,
+  price: state.books.filters.price,
+})
+
+const mapDispatchToProps = (dispatch) => ({
+  onChangeFilter: (name, value) => dispatch(showFilteredBooks(name, value)),
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Filters);
