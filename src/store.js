b9d1c5fb0577f91d72c88738d6da25a7d4d384e6
Serhii Babii <babiysergey1988@gmail.com>
2019-12-15 15:05:18 +0200

    FT-18: + save user to LocalSorage

--- src/store.js
+++ src/store.js
@@ -1,6 +1,14 @@
 import { createStore } from 'redux';
 import reducers from './reducers/reducers';
+import { saveState } from './localStorage/localStorage';
 
+// eslint-disable-next-line no-underscore-dangle
 const store = createStore(reducers);
+
+store.subscribe(() => {
+  saveState({
+    user: store.getState().books.user,
+  });
+});
 
 export default store;
